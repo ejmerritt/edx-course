b6cfb92334c750ba4755676275f26bd42d8c9029
ejm <ejm@disr.it>
2021-03-03 11:39:35 +0100

    notes for 4.2.3

--- bird_migration.py
+++ bird_migration.py
@@ -5,12 +5,14 @@ import numpy as np
 birddata = pd.read_csv("bird_tracking.csv")
 birddata.info()
 print(birddata.head())
+
 bird_names = pd.unique(birddata.bird_name)
 plt.figure(figsize = (7,7))
 for bird in bird_names:
-ix = birddata.bird_name ==bird_name
-x, y = birddata.longitude[ix], birddata.latitude[ix]
-plt.plot(x, y, ".")
+    ix = birddata.bird_name == bird
+    x, y = birddata.longitude[ix], birddata.latitude[ix]
+    plt.plot(x, y, ".")
 plt.xlabel("Longitude")
 plt.ylabel("Latitude")
-plt.legend(loc="lower right")
+#plt.legend(loc="lower right")
+speed = birddata.speed_2d[ix]
